f56c93c20a7cca4d5ada4f3fe4a922ae7dddf9ae
Admin <Admin@DESKTOP-G2QUO26>
2021-07-15 19:03:06 +0300

    Init project

---
+++ src/main/java/dna/test/medcenter/MedcenterApplication.java
@@ -0,0 +1,13 @@
+package dna.test.medcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedcenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedcenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dna/test/medcenter/MedcenterApplicationTests.java
@@ -0,0 +1,13 @@
+package dna.test.medcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedcenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

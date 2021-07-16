94ba99665abe99ed040f1c7225d0c8012d8a5eb0
Admin <Admin@DESKTOP-G2QUO26>
2021-07-16 12:13:23 +0300

    Landing/login page

---
+++ src/main/java/dna/test/medcenter/controllers/IndexController.java
@@ -0,0 +1,17 @@
+package dna.test.medcenter.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Basic view controller to enable reaching the main page
+ * @author GU
+ */
+@Controller
+public class IndexController {
+
+	@GetMapping("/")
+    public String redirectToIndexPage(){
+        return "/views/index.html";
+    }
+}
